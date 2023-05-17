60c488bfee35524e91efef0d6f1de03dddb595fa
adb9741 <18burrolaa@gmail.com>
2023-05-16 22:41:38 -0500

    got in default header + basic home

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,21 +1,83 @@
-import React from "react";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import { useState } from "react";
 
-const Header = () => {
+const items = [
+  {
+    label: "Navigation One",
+    key: "mail",
+    icon: <MailOutlined />,
+  },
+  {
+    label: "Navigation Two",
+    key: "app",
+    icon: <AppstoreOutlined />,
+    disabled: true,
+  },
+  {
+    label: "Navigation Three - Submenu",
+    key: "SubMenu",
+    icon: <SettingOutlined />,
+    children: [
+      {
+        type: "group",
+        label: "Item 1",
+        children: [
+          {
+            label: "Option 1",
+            key: "setting:1",
+          },
+          {
+            label: "Option 2",
+            key: "setting:2",
+          },
+        ],
+      },
+      {
+        type: "group",
+        label: "Item 2",
+        children: [
+          {
+            label: "Option 3",
+            key: "setting:3",
+          },
+          {
+            label: "Option 4",
+            key: "setting:4",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+    key: "alipay",
+  },
+];
+
+function Header() {
+  const [current, setCurrent] = useState("mail");
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
   return (
-    <React.StrictMode>
-      <header
-        style={{
-          backgroundColor: "#A348FA",
-          position: "fixed",
-          width: "100%",
-          height: "60px",
-          top: 0,
-        }}
-      >
-        <h1 style={{ margin: 0 }}>A heading here</h1>
-      </header>
-    </React.StrictMode>
+    <Menu
+      style={{ backgroundColor: "#A348FA" }}
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
   );
-};
+}
 
 export default Header;
